1690ee2720cf195f20e2ee0b6021ab49a49bbcad
leve2500 <43927045+leve2500@users.noreply.github.com>
2021-01-23 19:11:51 +0800

    公共定义文件上传

--- sgdevagent_V01/include/public_struct.h
+++ sgdevagent_V01/include/public_struct.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) Huawei Technologies Co., Ltd. 2019-2020. All rights reserved.
- * Description : sgdevagent common header file
+ * Description : sgdevagent public header file
 */
 
 
